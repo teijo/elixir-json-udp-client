5e49c7b3adbe0ebebaf06b85d0f555c2df35c005
Teijo Laine <teijo@aropupu.fi>
2015-05-06 23:54:14 +0300

    Parse and pattern match JSON

--- lib/json_cli.ex
+++ lib/json_cli.ex
@@ -1,7 +1,19 @@
+defmodule Body do
+  defstruct [:boolean, :string, :number]
+end
+
+defmodule Message do
+  defstruct [:type, body: Body]
+end
+
 defmodule JsonCli do
   defp read(server) do
     { data, client } = server |> Socket.Datagram.recv!
-    server |> Socket.Datagram.send! data, client
+    message = Poison.decode!(data, as: Message)
+    case message do
+      %Message{type: "test", body: body} -> server |> Socket.Datagram.send!(~s([#{body["boolean"]}, "#{body["string"]}", #{body["number"]}]), client)
+      _ -> server |> Socket.Datagram.send!("got unexpected message", client)
+    end
     read(server)
   end
 
--- test/json_cli_test.exs
+++ test/json_cli_test.exs
@@ -1,11 +1,19 @@
 defmodule JsonCliTest do
   use ExUnit.Case
 
-  test "sent data is echoed back" do
-    spawn fn -> JsonCli.listen() end
+  spawn fn -> JsonCli.listen() end
+
+  test "sent data is parsed and sent back" do
     server = Socket.UDP.open!(7331)
-    server |> Socket.Datagram.send! "foobar", { {127, 0, 0, 1}, 1337 }
+    server |> Socket.Datagram.send! ~s({"type": "test", "body": {"boolean": true, "string": "foobar", "number": 3}}), { {127, 0, 0, 1}, 1337 }
     { data, _ } = server |> Socket.Datagram.recv!
-    assert data == "foobar"
+    assert data == ~s([true, "foobar", 3])
+  end
+
+  test "invalid message" do
+    server = Socket.UDP.open!(7331)
+    server |> Socket.Datagram.send! ~s({}), { {127, 0, 0, 1}, 1337 }
+    { data, _ } = server |> Socket.Datagram.recv!
+    assert data == "got unexpected message"
   end
 end
