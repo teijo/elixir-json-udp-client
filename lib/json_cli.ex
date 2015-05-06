9356ac14433b938194a651ce7f82da0e3ab8e0f2
Teijo Laine <teijo@aropupu.fi>
2015-05-06 23:00:11 +0300

    Test message echoing over UDP

--- lib/json_cli.ex
+++ lib/json_cli.ex
@@ -1,8 +1,6 @@
 defmodule JsonCli do
   defp read(server) do
-    IO.puts("Waiting for data")
     { data, client } = server |> Socket.Datagram.recv!
-    IO.puts("Received: #{data}")
     server |> Socket.Datagram.send! data, client
     read(server)
   end
--- test/json_cli_test.exs
+++ test/json_cli_test.exs
@@ -1,7 +1,11 @@
 defmodule JsonCliTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "sent data is echoed back" do
+    spawn fn -> JsonCli.listen() end
+    server = Socket.UDP.open!(7331)
+    server |> Socket.Datagram.send! "foobar", { {127, 0, 0, 1}, 1337 }
+    { data, _ } = server |> Socket.Datagram.recv!
+    assert data == "foobar"
   end
 end
